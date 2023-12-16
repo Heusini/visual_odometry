f2b68f08da588d2e2b2b69b32848e031864b2e68
Sebastian Heusinger <heusings@googlemail.com>
2023-12-16 13:47:30 +0100

    cleanup of camera_pose

--- vo_pipeline/camera_pose.py
+++ vo_pipeline/camera_pose.py
@@ -3,9 +3,6 @@ from typing import List
 
 import cv2 as cv
 import numpy as np
-from exercise_helpers.decompose_essential_matrix import decomposeEssentialMatrix
-from exercise_helpers.disambiguate_relative_pose import disambiguateRelativePose
-from exercise_helpers.normalise_2D_pts import normalise2DPts
 
 
 class DataSetEnum(Enum):
@@ -40,152 +37,88 @@ def essential_matrix_from_fundamental_matrix(fundamental_mat, K):
     return K.T @ fundamental_mat @ K
 
 
-def plot_plotly(P, cameras: List[Camera]):
-    import plotly.graph_objects as go
-    from plotly.subplots import make_subplots
-
-    plotly_fig = make_subplots(
-        rows=1, cols=2, specs=[[{"type": "scatter"}, {"type": "scatter3d"}]]
-    )
-    # draw 2D xz scatter plot plus camera position
-    scatter_2D = go.Scatter(
-        x=P[0, :],
-        y=P[2, :],
-        mode="markers",
-        marker=dict(
-            size=3,
-            color="red",  # set color to an array/list of desired values
-            colorscale="Viridis",  # choose a colorscale
-            opacity=0.8,
-        ),
-    )
-    plotly_fig.add_trace(scatter_2D, 1, 1)
-
-    center_point_C1 = cameras[0].rotation.T @ (-cameras[0].translation[:, 0])
-    center_point_C2 = cameras[1].rotation.T @ (-cameras[1].translation[:, 0])
-    camera_poses = go.Scatter(
-        x=[center_point_C1[0], center_point_C2[0]],
-        y=[center_point_C1[2], center_point_C2[2]],
-        mode="markers+text",
-        textposition="bottom center",
-        text=["C1", "C2"],
-        marker=dict(size=5, color="blue", colorscale="Viridis", opacity=0.8, symbol=4),
-    )
-    plotly_fig.add_trace(camera_poses, 1, 1)
-    # draw 3D scatter plot plus camera frames
-    scatter_3d = go.Scatter3d(
-        x=P[0, :],
-        y=P[1, :],
-        z=P[2, :],
-        mode="markers",
-        marker=dict(
-            size=3,
-            color="red",  # set color to an array/list of desired values
-            colorscale="Viridis",  # choose a colorscale
-            opacity=0.8,
-        ),
-    )
-    plotly_fig.add_trace(scatter_3d, 1, 2)
-    colors = ["black", "green", "blue", "yellow", "orange", "purple", "pink", "brown"]
-    count = 0
-    for camera in cameras:
-        camera_wireframe = draw_camera_wireframe(
-            camera.rotation,
-            camera.translation,
-            0.5,
-            0.5,
-            camera.name,
-            colors[count],
-        )
-        count = (count + 1) % len(colors)
-        for line in camera_wireframe:
-            plotly_fig.add_trace(line, 1, 2)
-    plotly_fig.show()
-
-
-def draw_camera_wireframe(rotation, translation, f, size, cam_name, color="black"):
-    import plotly.graph_objects as go
-
-    p1_c = np.array([-size / 2, -size / 2, f])
-    p2_c = np.array([size / 2, -size / 2, f])
-    p3_c = np.array([size / 2, size / 2, f])
-    p4_c = np.array([-size / 2, size / 2, f])
-
-    p1_w = rotation.T @ (p1_c - translation[:, 0])
-    p2_w = rotation.T @ (p2_c - translation[:, 0])
-    p3_w = rotation.T @ (p3_c - translation[:, 0])
-    p4_w = rotation.T @ (p4_c - translation[:, 0])
-
-    center_point = rotation.T @ (-translation[:, 0])
-    print(center_point.shape)
-
-    # draw camera wireframe
-    camera_wireframe = go.Scatter3d(
-        x=[p1_w[0], p2_w[0], p3_w[0], p4_w[0], p1_w[0]],
-        y=[p1_w[1], p2_w[1], p3_w[1], p4_w[1], p1_w[1]],
-        z=[p1_w[2], p2_w[2], p3_w[2], p4_w[2], p1_w[2]],
-        mode="lines",
-        name=cam_name,
-        line=dict(color=color, width=4),
-        legendgroup=cam_name,
-        showlegend=True,
-    )
-
-    center_line1 = go.Scatter3d(
-        x=[center_point[0], p1_w[0]],
-        y=[center_point[1], p1_w[1]],
-        z=[center_point[2], p1_w[2]],
-        mode="lines",
-        name=cam_name + "line1",
-        line=dict(color=color, width=4),
-        legendgroup=cam_name,
-        showlegend=False,
-    )
-    center_line2 = go.Scatter3d(
-        x=[center_point[0], p2_w[0]],
-        y=[center_point[1], p2_w[1]],
-        z=[center_point[2], p2_w[2]],
-        mode="lines",
-        name=cam_name + "line2",
-        line=dict(color=color, width=4),
-        legendgroup=cam_name,
-        showlegend=False,
-    )
-    center_line3 = go.Scatter3d(
-        x=[center_point[0], p3_w[0]],
-        y=[center_point[1], p3_w[1]],
-        z=[center_point[2], p3_w[2]],
-        mode="lines",
-        name=cam_name + "line3",
-        line=dict(color=color, width=4),
-        legendgroup=cam_name,
-        showlegend=False,
-    )
-    center_line4 = go.Scatter3d(
-        x=[center_point[0], p4_w[0]],
-        y=[center_point[1], p4_w[1]],
-        z=[center_point[2], p4_w[2]],
-        mode="lines",
-        name=cam_name + "line4",
-        line=dict(color=color, width=4),
-        legendgroup=cam_name,
-        showlegend=False,
-    )
-
-    lines = [camera_wireframe, center_line1, center_line2, center_line3, center_line4]
-
-    return lines
-
-
+# test stuff
 if __name__ == "__main__":
-    import os
-    import sys
     import time
 
     from correspondence import correspondence
-    from exercise_helpers.camera_3d import drawCamera
+    from exercise_helpers.decompose_essential_matrix import decomposeEssentialMatrix
+    from exercise_helpers.disambiguate_relative_pose import disambiguateRelativePose
     from exercise_helpers.linear_triangulation import linearTriangulation
-    from helpers import load_images
+    from helpers import draw_camera_wireframe
+
+    def plot_plotly(P, cameras: List[Camera]):
+        import plotly.graph_objects as go
+        from plotly.subplots import make_subplots
+
+        plotly_fig = make_subplots(
+            rows=1, cols=2, specs=[[{"type": "scatter"}, {"type": "scatter3d"}]]
+        )
+        # draw 2D xz scatter plot plus camera position
+        scatter_2D = go.Scatter(
+            x=P[0, :],
+            y=P[2, :],
+            mode="markers",
+            marker=dict(
+                size=3,
+                color="red",  # set color to an array/list of desired values
+                colorscale="Viridis",  # choose a colorscale
+                opacity=0.8,
+            ),
+        )
+        plotly_fig.add_trace(scatter_2D, 1, 1)
+
+        center_point_C1 = cameras[0].rotation.T @ (-cameras[0].translation[:, 0])
+        center_point_C2 = cameras[1].rotation.T @ (-cameras[1].translation[:, 0])
+        camera_poses = go.Scatter(
+            x=[center_point_C1[0], center_point_C2[0]],
+            y=[center_point_C1[2], center_point_C2[2]],
+            mode="markers+text",
+            textposition="bottom center",
+            text=["C1", "C2"],
+            marker=dict(
+                size=5, color="blue", colorscale="Viridis", opacity=0.8, symbol=4
+            ),
+        )
+        plotly_fig.add_trace(camera_poses, 1, 1)
+        # draw 3D scatter plot plus camera frames
+        scatter_3d = go.Scatter3d(
+            x=P[0, :],
+            y=P[1, :],
+            z=P[2, :],
+            mode="markers",
+            marker=dict(
+                size=3,
+                color="red",  # set color to an array/list of desired values
+                colorscale="Viridis",  # choose a colorscale
+                opacity=0.8,
+            ),
+        )
+        plotly_fig.add_trace(scatter_3d, 1, 2)
+        colors = [
+            "black",
+            "green",
+            "blue",
+            "yellow",
+            "orange",
+            "purple",
+            "pink",
+            "brown",
+        ]
+        count = 0
+        for camera in cameras:
+            camera_wireframe = draw_camera_wireframe(
+                camera.rotation,
+                camera.translation,
+                0.5,
+                0.5,
+                camera.name,
+                colors[count],
+            )
+            count = (count + 1) % len(colors)
+            for line in camera_wireframe:
+                plotly_fig.add_trace(line, 1, 2)
+        plotly_fig.show()
 
     imgs = []
     # imgs = load_images("data/kitti/05/image_0/", start=0, end=2)
@@ -270,26 +203,3 @@ if __name__ == "__main__":
     cameras.append(Camera(R_C2_W, T_C2_W, K, "Cam 2"))
 
     plot_plotly(P, cameras)
-
-    # # plot keypoints from top view and camera
-    # import matplotlib.pyplot as plt
-    # from exercise_helpers.arrow_3d import Arrow3D
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection="3d")
-    # ax.scatter(P[0, :], P[1, :], P[2, :], marker="o", s=4)
-    # ax.set_xlabel("X Label")
-    # ax.set_ylabel("Y Label")
-    # ax.set_zlabel("Z Label")
-    #
-    # # Display camera pose
-    # drawCamera(ax, np.zeros((3,)), np.eye(3), length_scale=2)
-    # ax.text(-0.1, -0.1, -0.1, "Cam 1")
-    #
-    # center_cam2_W = -R_C2_W.T @ T_C2_W
-    # drawCamera(ax, center_cam2_W, R_C2_W.T, length_scale=2)
-    # ax.text(
-    #     center_cam2_W[0] - 0.1, center_cam2_W[1] - 0.1, center_cam2_W[2] - 0.1, "Cam 2"
-    # )
-    #
-    # plt.show()
--- vo_pipeline/helpers.py
+++ vo_pipeline/helpers.py
@@ -1,15 +1,16 @@
-from typing import Optional, Callable
-import numpy as np
-import cv2
 import os
+from typing import Callable, Optional
+
+import cv2
+import numpy as np
+
 
 def load_images(
     path: str,
-    filter : Optional[Callable[[str], bool]] = None,
-    start : Optional[int] = None,
-    end : Optional[int] = None
+    filter: Optional[Callable[[str], bool]] = None,
+    start: Optional[int] = None,
+    end: Optional[int] = None,
 ) -> np.ndarray:
-    
     filenames = []
 
     # open directory and read all filenames
@@ -21,8 +22,10 @@ def load_images(
     # sort filenames
     filenames.sort()
 
-    if start is None: start = 0
-    if end is None: end = len(filenames)
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(filenames)
 
     # load images
     imgs = []
@@ -32,3 +35,77 @@ def load_images(
         imgs.append(img)
 
     return np.array(imgs)
+
+
+def draw_camera_wireframe(rotation, translation, f, size, cam_name, color="black"):
+    import plotly.graph_objects as go
+
+    p1_c = np.array([-size / 2, -size / 2, f])
+    p2_c = np.array([size / 2, -size / 2, f])
+    p3_c = np.array([size / 2, size / 2, f])
+    p4_c = np.array([-size / 2, size / 2, f])
+
+    p1_w = rotation.T @ (p1_c - translation[:, 0])
+    p2_w = rotation.T @ (p2_c - translation[:, 0])
+    p3_w = rotation.T @ (p3_c - translation[:, 0])
+    p4_w = rotation.T @ (p4_c - translation[:, 0])
+
+    center_point = rotation.T @ (-translation[:, 0])
+    print(center_point.shape)
+
+    # draw camera wireframe
+    camera_wireframe = go.Scatter3d(
+        x=[p1_w[0], p2_w[0], p3_w[0], p4_w[0], p1_w[0]],
+        y=[p1_w[1], p2_w[1], p3_w[1], p4_w[1], p1_w[1]],
+        z=[p1_w[2], p2_w[2], p3_w[2], p4_w[2], p1_w[2]],
+        mode="lines",
+        name=cam_name,
+        line=dict(color=color, width=4),
+        legendgroup=cam_name,
+        showlegend=True,
+    )
+
+    center_line1 = go.Scatter3d(
+        x=[center_point[0], p1_w[0]],
+        y=[center_point[1], p1_w[1]],
+        z=[center_point[2], p1_w[2]],
+        mode="lines",
+        name=cam_name + "line1",
+        line=dict(color=color, width=4),
+        legendgroup=cam_name,
+        showlegend=False,
+    )
+    center_line2 = go.Scatter3d(
+        x=[center_point[0], p2_w[0]],
+        y=[center_point[1], p2_w[1]],
+        z=[center_point[2], p2_w[2]],
+        mode="lines",
+        name=cam_name + "line2",
+        line=dict(color=color, width=4),
+        legendgroup=cam_name,
+        showlegend=False,
+    )
+    center_line3 = go.Scatter3d(
+        x=[center_point[0], p3_w[0]],
+        y=[center_point[1], p3_w[1]],
+        z=[center_point[2], p3_w[2]],
+        mode="lines",
+        name=cam_name + "line3",
+        line=dict(color=color, width=4),
+        legendgroup=cam_name,
+        showlegend=False,
+    )
+    center_line4 = go.Scatter3d(
+        x=[center_point[0], p4_w[0]],
+        y=[center_point[1], p4_w[1]],
+        z=[center_point[2], p4_w[2]],
+        mode="lines",
+        name=cam_name + "line4",
+        line=dict(color=color, width=4),
+        legendgroup=cam_name,
+        showlegend=False,
+    )
+
+    lines = [camera_wireframe, center_line1, center_line2, center_line3, center_line4]
+
+    return lines
