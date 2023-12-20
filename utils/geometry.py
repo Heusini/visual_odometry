4b082c11be5591a56f646f328c62a6663cfb4df4
hangchen <hangchen.xie@uzh.ch>
2023-12-20 17:31:06 +0100

    add geometry.py

---
+++ utils/geometry.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+def calculate_fundamental_matrix(points1, points2):
+    F, mask = cv2.findFundamentalMat(points1, points2, cv2.RANSAC, 1, 0.9999, 50000)
+    return F, mask
+
+def calculate_essential_matrix(points1, points2, K):
+    E, mask_e = cv2.findEssentialMat(points1, points2, K, cv2.RANSAC, 0.9999, 1)
+    return E, mask_e
+
+def calculate_essential_matrix_from_fundamental_matrix(F, K):
+    return K.T @ F @ K
