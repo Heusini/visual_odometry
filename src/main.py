c1aae25fe41a5ea54dbd9223184ba9a4200be4c3
Riccardo Feingold <riccardofeingold@gmail.com>
2023-12-25 17:57:58 +0200

    updated main file
    it uses now the dataloader; it has a initialization part; currently continuous part is the same as in the pnp.py

--- src/main.py
+++ src/main.py
@@ -1,48 +1,60 @@
-from enum import Enum
-import cv2 as cv
-import numpy as np
-from state import FrameSate
-from utils.path_loader import PathLoader
 from pnp import pnp
+from utils.dataloader import DataLoader, Dataset
+from initialization import initialize
 
 # Constants
 init_bandwidth = 4
 
-class DataSetEnum(Enum):
-    KITTI = "kitti"
-    PARKING = "parking"
-    MALAGA = "malaga"
-
 def main():
+    # select dataset
+    dataset = Dataset.MALAGA
     
-    dataset = DataSetEnum.KITTI
+    # load data
+    # if you remove steps then all the images are used
+    loader = DataLoader(dataset, start=90, stride=1, steps=90)
+    print("Loading data...")
 
-    K_kitty = np.array(
-        [
-            [7.188560000000e02, 0, 6.071928000000e02],
-            [0, 7.188560000000e02, 1.852157000000e02],
-            [0, 0, 1],
-        ]
-    )
-    K_parking = np.array([[331.37, 0, 320], [0, 369.568, 240], [0, 0, 1]])
+    loader.load_data()
+    print("Data loaded!")
 
-    if dataset == DataSetEnum.KITTI:
-        K = K_kitty
-        path = "data/kitti/05/image_0/"
-    elif dataset == DataSetEnum.PARKING:
-        K = K_parking
-        path = "data/parking/images/"
-
-    path_loader = PathLoader(path, start=0, stride=init_bandwidth)
-    path_iter = iter(path_loader)
-
-    # initialize first two states, corresponding features are computed with initialization
-    state_0 = FrameSate(0, next(path_iter))
-    state_1 = FrameSate(init_bandwidth, next(path_iter))
-
+    K, poses, states = loader.get_data()
+    print("Data retrieved!")
+    
+    # TODO: use some sort of params variable to store the best parameters
+    # for each dataset; Maybe add this to the dataloader
+    
     # computes landmarks and pose for the first two frames
-    pnp(state_0, state_1, K)
+    if dataset == Dataset.KITTI:
+        initialize(states[0], states[3], K)
+        init_states = [states[0], states[3]]
+    elif dataset == Dataset.PARKING:
+        initialize(states[0], states[3], K)
+        init_states = [states[0], states[3]]
+    elif dataset == Dataset.MALAGA:
+        initialize(states[0], states[4], K)
+        init_states = [states[0], states[4]]
+    elif dataset == Dataset.WOKO:
+        initialize(states[0], states[3], K)
+        init_states = [states[0], states[3]]
+
+    plot_points_cameras(
+        [init_states[0].landmarks, init_states[1].landmarks],
+        [init_states[0].cam_to_world, init_states[1].cam_to_world]
+    )
     
+    # TODO: Implement Continuous Part: Feel free to change
+    start = 0
+    steps = 30
+    stride = 1
+    for i in range(start, steps-1):
+        pnp(states[i], states[i+stride], K)
+
+    plot_points_cameras(
+        [state.landmarks for state in states[0:steps-1:stride]],
+        [state.cam_to_world for state in states[0:steps-1:stride]],
+        plot_points=False
+    )
 
 if __name__ == '__main__':
+    from plot_points_cameras import plot_points_cameras
     main()
