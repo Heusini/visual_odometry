9e65cc046249b324575b46e4e346ab725e350407
Riccardo Feingold <riccardofeingold@gmail.com>
2023-12-27 02:09:05 +0200

    first version of keypoint tracking
    currently there the new landmark method has some mathematical errors. First error I spotted was the angle calculation.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from twoDtwoD import twoDtwoD
 from utils.dataloader import DataLoader, Dataset
 from initialization import initialize
 from pnp import pnp
+from tracking import Tracking
+import numpy as np
+import cv2 as cv
 
 # Constants
 init_bandwidth = 4
@@ -28,15 +31,19 @@ def main():
     if dataset == Dataset.KITTI:
         initialize(states[0], states[3], K)
         init_states = [states[0], states[3]]
+        init_frame_indices = [0, 3]
     elif dataset == Dataset.PARKING:
         initialize(states[0], states[3], K)
         init_states = [states[0], states[3]]
+        init_frame_indices = [0, 3]
     elif dataset == Dataset.MALAGA:
         initialize(states[0], states[4], K)
         init_states = [states[0], states[4]]
+        init_frame_indices = [0, 4]
     elif dataset == Dataset.WOKO:
         initialize(states[0], states[3], K)
         init_states = [states[0], states[3]]
+        init_frame_indices = [0, 3]
 
     plot_points_cameras(
         [init_states[0].landmarks, init_states[1].landmarks],
@@ -44,28 +51,29 @@ def main():
     )
     
     # TODO: Implement Continuous Part: Feel free to change
+    tracking = Tracking(angle_threshold=np.pi/100, init_frame_indices=init_frame_indices)
     steps = 0
     while steps < len(states) - 3:
-        if steps == 90:
+        if steps == 21:
             break
         
-        # if only 20 % of the landmarks are visible, call initialize again
-        # currently it is not adding the keypoints and landmarks but rather replacing the current landmarks and kp
-        # this step should be then removed with KLT where we add new keypoints at each frame
-        # (probably a good idea is to store this in a separate variable in framestate, since 
-        # this newly added keypoints can only be used at the point where new landmarks are created
-        # see project statement last page)
-        # TODO: Implement 4.3 (see project statement)
-        if states[steps].landmarks.shape[1] / states[0].landmarks.shape[1] < 0.2:
-            initialize(states[steps], states[steps + 3], K)
+        # add new keypoints for current frame t
+        tracking.add_new_keypoint_candidates(states[steps].features.keypoints, start_frame_index=states[steps].t)
 
+        # track existing keypoints
+        tracking.track_keypoints(img_i=cv.imread(states[steps].img_path), img_j=cv.imread(states[steps+1].img_path))
+
+        # check if new landmarks can be created and if so do it
+        tracking.check_for_new_landmarks(frame_states=states, K=K)
+
+        # calculate the camera pose from the information given at frame i and i + 1
         pnp(states[steps], states[steps+1], K)
         steps += 1
 
     plot_points_cameras(
         [state.landmarks for state in states[0:steps-1]],
         [state.cam_to_world for state in states[0:steps-1]],
-        plot_points=False
+        plot_points=True
     )
 
 if __name__ == '__main__':
---
+++ src/tracking.py
@@ -0,0 +1,96 @@
+import cv2 as cv
+import numpy as np
+from state import FrameState
+from utils.linear_triangulation import linearTriangulation
+from klt import klt
+
+class Tracking:
+    def __init__(self, angle_threshold: float, init_frame_indices: [int, int]) -> None:
+        self.tracks = {}
+        self.start_frame_indices = []
+        self.angle_threshold = angle_threshold
+        self.init_frame_indices = init_frame_indices
+        pass
+    
+    def add_new_keypoint_candidates(self, keypoint_candidates: np.ndarray, start_frame_index: int):
+        if start_frame_index not in self.init_frame_indices:
+            self.tracks[start_frame_index] = [np.array([kp.pt for kp in keypoint_candidates])]
+            self.start_frame_indices.append(start_frame_index)
+        pass
+
+    def track_keypoints(self, img_i, img_j):
+        for start_frame_index in self.start_frame_indices:
+            if len(self.tracks[start_frame_index][0]) == 0:
+                self.tracks.pop(start_frame_index)
+                self.start_frame_indices.remove(start_frame_index)
+                continue
+
+            kp_j, mask = klt(self.tracks[start_frame_index][-1], img_i, img_j)
+            mask_indices = np.where(mask.reshape(-1, 1) == True)[0]
+            kp_j = kp_j[mask_indices].squeeze()
+            if len(kp_j.shape) == 1:
+                kp_j = kp_j.reshape(1, -1)
+
+            # select only the keypoints based on the good ones of current
+            # kp_j tracking
+            self.tracks[start_frame_index] = [track[mask_indices] for track in self.tracks[start_frame_index]]
+            self.tracks[start_frame_index].append(kp_j)
+
+    def check_for_new_landmarks(self, frame_states: FrameState, K: np.ndarray) -> bool:
+        for start_frame_index in self.start_frame_indices:
+            track = self.tracks[start_frame_index]
+            end_frame_index = start_frame_index + len(track) - 1
+
+            K_inv = np.linalg.inv(K)
+            homogenous_start_track = np.vstack([track[0].T, np.ones((1, track[0].shape[0]))])
+            homogenous_end_track = np.vstack([track[-1].T, np.ones((1, track[-1].shape[0]))])
+            
+            c_P_start = K_inv @ homogenous_start_track
+            c_P_end = K_inv @ homogenous_end_track
+
+            dot_products = np.diag(np.dot(c_P_start.T, c_P_end))
+            start_norms = np.linalg.norm(c_P_start, axis=0)
+            end_norms = np.linalg.norm(c_P_end, axis=0)
+            norm_product = start_norms * end_norms
+            cos_angles = dot_products / norm_product
+            angles = np.array([np.arccos(cs_angle) for cs_angle in cos_angles])
+            mask_angle = np.where(angles.reshape(-1, 1) > self.angle_threshold)[0]
+
+            cam_start_to_world = frame_states[start_frame_index].cam_to_world
+            cam_end_to_world = frame_states[end_frame_index].cam_to_world
+
+            M_cam_start_cam_end = np.linalg.inv(cam_start_to_world) @ cam_end_to_world
+
+            p_start = homogenous_start_track[:, mask_angle]
+            p_end = homogenous_end_track[:, mask_angle]
+
+            P_cam_start = linearTriangulation(
+                p_start, 
+                p_end, 
+                K @ np.eye(3, 4),
+                K @ M_cam_start_cam_end[0:3, :])
+            
+            #filter points behind camera and far away
+            max_distance = 100
+            mask_filter = np.logical_and(P_cam_start[2, :] > 0, np.abs(np.linalg.norm(P_cam_start, axis=0)) < max_distance)
+            P_cam_start = P_cam_start[:, mask_filter]
+            p_start = p_start[:, mask_filter]
+            p_end = p_end[:, mask_filter]
+
+            P_world = cam_start_to_world @ P_cam_start
+
+            ext_landmarks = np.hstack([frame_states[start_frame_index].landmarks, P_world[:3, :]])
+            frame_states[start_frame_index].landmarks = ext_landmarks
+            frame_states[end_frame_index].landmarks = ext_landmarks
+
+            # updating the keypoints in each state since there have been multiple
+            # filtering steps that reduced the number of initial keypoints
+            ext_keypoints_start = np.hstack([frame_states[start_frame_index].keypoints, p_start[:2, :]])
+            ext_keypoints_end = np.hstack([frame_states[start_frame_index].keypoints, p_end[:2, :]])
+            frame_states[start_frame_index].keypoints = ext_keypoints_start
+            frame_states[end_frame_index].keypoints = ext_keypoints_end
+
+            # remove track since it is now used as landmarks
+            self.tracks[start_frame_index] = [track[mask_angle] for track in self.tracks[start_frame_index]]
+            return frame_states
+        pass
